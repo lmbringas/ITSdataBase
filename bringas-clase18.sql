739bb8af0a7f4194d500eece7ea03917f0aec4ac
lmbringas <lm.bringas98@gmil.com>
2017-08-17 15:29:18 -0300

    add 19

--- bringas-clase18.sql
+++ bringas-clase18.sql
@@ -5,3 +5,16 @@ CALL CountOrderByStatus('Shipped',@total);
 SELECT @total;
 CALL CountOrderByStatus('in process',@total);
 SELECT @total AS  total_in_process;
+CALL get_order_by_cust(141,@shipped,@canceled,@resolved,@disputed);
+SELECT @shipped,@canceled,@resolved,@disputed;
+SET @customerNo = 112;
+
+SELECT country into @country
+FROM customers
+WHERE customernumber = @customerNo;
+
+CALL GetCustomerShipping(@customerNo,@shipping);
+
+SELECT @customerNo AS Customer,
+       @country    AS Country,
+       @shipping   AS Shipping;
---
+++ bringas-clase19.sql
@@ -0,0 +1,56 @@
+call test_mysql_loop();
+SET @email_list = "";
+CALL build_email_list(@email_list);
+SELECT @email_list;
+
+-- 1 
+SELECT COUNT(*) FROM film
+INNER JOIN inventory USING(film_id)
+INNER JOIN store USING(store_id)
+WHERE store_id = 2
+AND (film_id = 3  OR title LIKE "Coma Head");
+
+
+CREATE FUNCTION GetCountFilm(p_storeId INTEGER, p_filmId INTEGER, p_title VARCHAR(20)) RETURNS INTEGER
+    DETERMINISTIC
+BEGIN
+    DECLARE total INTEGER;
+    SET total = (SELECT COUNT(film_id) FROM film
+		INNER JOIN inventory USING(film_id)
+		INNER JOIN store USING(store_id)
+		WHERE store_id = p_storeId
+			AND film_id = p_filmId 
+			AND title LIKE p_title);
+    RETURN (total);
+  END
+  
+
+
+SELECT GetCountFilm(2,3,"Coma Head");
+
+-- 2 
+-- Write a stored procedure that returns a list of customer first and last names separated by ";", 
+-- that live in a certain country. You pass the country it gives you the list of people living there.
+SELECT GROUP_CONCAT(first_name, " " ,last_name SEPARATOR "; ") FROM customer
+	INNER JOIN address USING(address_id)
+	INNER JOIN city USING(city_id)
+	INNER JOIN country USING(country_id);
+	
+CALL GetListCustomer("Argentina", @p_customerList);
+SELECT @p_customerList;
+
+CREATE DEFINER=`root`@`%` PROCEDURE `sakila`.`GetListCustomer`(
+    IN  p_countryName VARCHAR(24),
+    OUT p_customerList  TEXT
+)
+BEGIN
+   SELECT GROUP_CONCAT(first_name, " " ,last_name SEPARATOR "; ") INTO p_customerList FROM customer
+	INNER JOIN address USING(address_id)
+	INNER JOIN city USING(city_id)
+	INNER JOIN country USING(country_id)
+	WHERE country = p_countryName;
+END
+
+set @List = "";
+CALL GetListCustomer("Argentina", @List);
+SELECT @List;
